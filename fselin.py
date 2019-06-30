899098e464e37b5b037f5307507c5a8aa6210008
unleashedcode <unleashedcode@gmail.com>
2019-06-30 10:51:58 +0000

    urwid menu beginnings

--- fselin.py
+++ fselin.py
@@ -4,13 +4,12 @@ import urwid
 import menu
 
 
-def mainmenu(title, dict_menu):
-
+def mainmenu(title, menuchoices):
     #print out the menu (urwid)
     body = [urwid.Text(title), urwid.Divider()]
-    for choice in dict_menu:
+    for choice in menuchoices:
         button = urwid.Button(choice)
-        urwid.connect_signal(button, click, items_selected, choice)
+        urwid.connect_signal(button, 'click', item_chosen, choice)
         body.append(urwid.AttrMap(button, None, focus_map='reversed'))
     return urwid.ListBox(urwid.SimpleFocusListWalker(body))
 
@@ -19,7 +18,7 @@ def item_chosen(button, choice):
     done = urwid.Button(u'OK')
     urwid.connect_signal(done, 'click', exit_program)
     main.original_widget = urwid.Filler(urwid.Pile([response,
-        urwid,AttrMap(done, None, focus_map='reversed')]))
+        urwid.AttrMap(done, None, focus_map='reversed')]))
 
 def exit_program(button):
     raise urwid.ExitMainLoop()
@@ -27,11 +26,12 @@ def exit_program(button):
 
 
 # load the menu (main menu)
-title = 'FSE-LIN 0.01'
+title = 'FSELIN 0.01'
 dictMainmenu = {1: 'Aircraft Sales', 2: 'Airport Information', 3: 'Your Hanger',
         4: 'Exit'}
+menuOptions = ['Aircraft Sales', 'Airport Information', 'Your Hanger', 'Exit']
 
-main = urwid.Padding(menu.mainmenu(title, dictMainmenu), left=2, right=2)
+main = urwid.Padding(mainmenu(title, menuOptions), left=2, right=2)
 top = urwid.Overlay(main, urwid.SolidFill(u'\N{MEDIUM SHADE}'),
         align='center', width=('relative', 60),
         valign='middle', height=('relative', 60),
--- menu.py
+++ menu.py
@@ -5,13 +5,13 @@ import os
 import time
 
 # menu system - displays the menu
-def mainmenu(title, dictMainmenu):
+def mainmenu(title, menuchoices):
 
     #print out the menu (urwid)_
     body = [urwid.Text(title), urwid.Divider()]
-    for choice in dictMainmenu:
+    for choice in menuchoices:
         button = urwid.Button(choice)
-        urwid.connect_signal(button, click, item_selected, choice)
+        urwid.connect_signal(button, 'click', item_chosen, choice)
         body.append(urwid.AttrMap(button, None, focus_map='reversed'))
     return urwid.ListBox(urwid.SimpleFocusListWalker(body))
 
@@ -20,7 +20,7 @@ def item_chosen(button, choice):
     response = urwid.Text([u'You chose ', choice, u'\n'])
     done = urwid.Button(u'OK')
     urwid.connect_signal(done, 'click', exit_program)
-    main.original_widget = urwwid.Filler(urwid.Pile([response,
+    main.original_widget = urwid.Filler(urwid.Pile([response,
         urwid.AttrMap(done, None,focus_map='reversed')]))
 
 
